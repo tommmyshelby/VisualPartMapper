c04f52b3ee69e1ac8f27820c8d96403ea717dc50
Ketan <kayhere003@gmail.com>
2025-04-18 12:48:37 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ExportList from './components/MappedExports/ExportList';
+
 import VisualPartMapper from './components/VisualPartMapper';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Route for ExportList component */}
-        <Route path="/exports" element={<ExportList />} />
+  
 
         {/* Route for VisualPartMapper component */}
         <Route path="/" element={<VisualPartMapper />} />
